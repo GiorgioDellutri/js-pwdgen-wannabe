d2f9c866189fe1aceada1c4aaad9443bf5b616ce
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-18 16:18:49 +0100

    passord generator

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const nome = prompt ('Inserisci il tuo nome');
+const cognome = prompt ('Inserisci il tuo cognome')
+const colore = prompt ('Inserisci il tuo colore preferito')
+const numero = "22"
+
+const password = nome + cognome + " " +sdfsdnumero
+
+document.getElementById('password').innerHTML = 'La tua passord è ' + password; 
+
+console.log
