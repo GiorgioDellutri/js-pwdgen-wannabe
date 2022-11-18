16d609a06540c4b37cd3e6ecb5b6e80d51e2ed0f
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-18 16:33:38 +0100

    change number value

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let nome = prompt ('Inserisci il tuo nome');
 let cognome = prompt ('Inserisci il tuo cognome')
 let colore = prompt ('Inserisci il tuo colore preferito')
-let numero = "ciao"
+let numero = "22"
 
 const password = nome + cognome + colore + " " + numero
 
