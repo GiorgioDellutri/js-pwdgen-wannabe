17152b5295fb01baba4c7792a4c0a22ea9ec1506
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-18 18:15:21 +0100

    aesthetic tweaks

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let nome = prompt ('Inserisci il tuo nome');
 let cognome = prompt ('Inserisci il tuo cognome')
 let colore = prompt ('Inserisci il tuo colore preferito')
-let numero = "22"
+const numero = "22"
 
-const password = nome + cognome + colore + " " + numero
+const password = nome + cognome + colore + "" + numero
 
-document.getElementById('password').innerHTML = 'La tua password è ' + password; 
+document.getElementById('password').innerHTML = 'La tua password non sicura è: ' +password; 
 
 console.log(nome)
 console.log(cognome)
